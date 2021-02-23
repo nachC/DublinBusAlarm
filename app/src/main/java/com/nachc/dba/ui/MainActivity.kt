5eb716f338db37748e49c8d97850350337945927
nacho <nacho.itc@gmail.com>
2021-02-23 10:54:21 +0100

    Using ConstraintLayout instead of FragmentContainerView in activity_main

--- app/src/main/java/com/nachc/dba/ui/MainActivity.kt
+++ app/src/main/java/com/nachc/dba/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import android.app.NotificationManager
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
